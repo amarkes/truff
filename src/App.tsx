163b07d88d5d41f2ec2677f083deecc255b33d9b
amarkes <tmowna@gmail.com>
2025-04-09 16:08:54 -0300

    fix: center layout

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,8 @@ import { MiningProgress } from "./components/MiningProgress";
 function App() {
   return (
     <GameProvider>
-      <div className="justify-center w-full">
-        <main className="flex flex-col min-h-screen p-4 bg-gray-100 dark:bg-zinc-900 text-black dark:text-white transition-all">
+      <div className="justify-center content-center flex">
+        <main className="w-5/12 flex flex-col min-h-screen p-4 bg-gray-100 dark:bg-zinc-900 text-black dark:text-white transition-all">
           <h1 className="text-4xl font-bold text-center mb-6">
             🪨 Truff Miner
           </h1>
